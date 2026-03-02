5b7dfe47e5ef6b5c52874f12c40768240005ba59
Roberto Alsina <roberto.alsina@gmail.com>
2026-03-02 16:59:59 -0300

    chore: bump version to 0.4.0
    Features added since 0.3.0:
    - Fixed Kemal CLI args issue in entrypoint script
    - Exclude mangrullo containers from mass updates to prevent self-termination
    - Sort containers with updates first in web view

--- src/web.cr
+++ src/web.cr
@@ -9,7 +9,7 @@ require "./update_manager"
 require "./constants"
 
 module Mangrullo
-  VERSION = "0.3.0"
+  VERSION = "0.4.0"
 
   # Web server entry point
   Kemal.config.port = Mangrullo::Constants::Web::DEFAULT_PORT
